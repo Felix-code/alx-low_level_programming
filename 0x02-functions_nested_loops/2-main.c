1a3de9cc97f9b444ec2f8cdda9dced82523b7a64
Felix <mukahfelix@gmail.com>
2022-06-11 06:48:23 -0700

    new commit

--- 0x02-functions_nested_loops/2-main.c
+++ 0x02-functions_nested_loops/2-main.c
@@ -9,15 +9,6 @@
 int main(void)
 {
     print_alphabet_x10();
-    print_alphabet_x10();
-    print_alphabet_x10();
-    print_alphabet_x10();
-    print_alphabet_x10();
-    print_alphabet_x10();
-    print_alphabet_x10();
-    print_alphabet_x10();
-    print_alphabet_x10();
-    print_alphabet_x10();
 
     return (0);
 }
