b2c492aa56fddcef4b8335186fe7f2cf381d595f
Felix <mukahfelix@gmail.com>
2022-06-11 04:41:23 -0700

    a function that prints 10 times the alphabet, in lowercase, followed by a new line.

---
+++ 0x02-functions_nested_loops/2-main.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - a program to print alphabet 10x.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    print_alphabet_x10();
+    print_alphabet_x10();
+    print_alphabet_x10();
+    print_alphabet_x10();
+    print_alphabet_x10();
+    print_alphabet_x10();
+    print_alphabet_x10();
+    print_alphabet_x10();
+    print_alphabet_x10();
+    print_alphabet_x10();
+    return (0);
+}
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_alphabet - prints the alphabet in lowercase
+ */
+void print_alphabet(void)
+{
+        char alpha;
+
+        for (alpha = 'a'; alpha <= 'z'; alpha++)
+
+                _putchar(alpha);
+
+        _putchar('\n');
+}
+
