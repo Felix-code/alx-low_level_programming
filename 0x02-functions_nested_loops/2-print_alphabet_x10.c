2c117024fa4d86d25a6c00dd5029c3d6f2d31897
Felix-code <84644592+Felix-code@users.noreply.github.com>
2022-09-16 11:22:38 +0100

    updated

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - function that prints alphabet in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet_x10(void)
+{
+	int num = 0;
+
+	while (num < 10)
+	{
+		char alpha = 'a';
+
+		while (alpha <= 'z')
+		{
+			_putchar(alpha);
+			alpha++;
+		}
+	
+	
+	_putchar('\n');
+	num++;
+	
+	}
+
+}
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -1,2 +1,4 @@
 **A Project on functions and nested loops**
-- A program that prints alphabets in lowercase followed by a new line
+- A program that prints _putchar followed by a new line
+- A program that prints alphabets in lowercase.
+- A function that prints alphabets 10x
