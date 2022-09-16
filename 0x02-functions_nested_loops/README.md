80e74206ffdbd973447eaf3c6228ddb5627e1a96
Felix-code <84644592+Felix-code@users.noreply.github.com>
2022-09-16 09:22:42 +0100

    a funtion to print alphabet in lowercase characters

---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,2 @@
+**A Project on functions and nested loops**
+- A program that prints alphabets in lowercase followed by a new line
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,9 @@
+void _putchar(void)
+{
+	char alpha;
+	
+		for(alpha = 'a'; alpha <= 'z'; alpha++)
+		
+		putchar(alpha);
+		putchar('\n');
+}
