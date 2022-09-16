66660f41ac04e83ba8e54174ac3cc41a55cf85e2
Felix-code <84644592+Felix-code@users.noreply.github.com>
2022-09-16 11:01:57 +0100

    print alphabet in lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - funtion to print alphabet using putchar
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet(void)
+{
+	char alpha;
+
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+		_putchar(alpha);
+	_putchar('\n');
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,1 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/** Prototype of all functions
+ */
+
 int _putchar(char alpha);
+void print_alphabet(void);
+
+#endif
