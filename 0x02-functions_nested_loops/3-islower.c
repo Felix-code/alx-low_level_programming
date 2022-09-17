3d1f7fddc8d22bbf155f77d2170bed135832b098
Felix-code <84644592+Felix-code@users.noreply.github.com>
2022-09-17 11:53:42 +0100

    solution

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,7 +3,7 @@
 #include <ctype.h>
 /**
  * _islower - Check for lowercase characters
- * @c - letter to be checked
+ * @c: character to be checked
  *
  * Return: 1 if letter is lowercase and 0 if otherwise
  */
