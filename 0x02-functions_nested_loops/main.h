f3056430b4c6da475e91c87b9b8728221abc1ec4
Felix-code <84644592+Felix-code@users.noreply.github.com>
2022-09-20 16:34:01 +0100

    print natural numbers

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,6 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdio.h>
 /** Prototype of all functions
  */
 
@@ -15,5 +16,6 @@ int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
 int add(int, int);
+void print_to_98(int n);
 
 #endif
