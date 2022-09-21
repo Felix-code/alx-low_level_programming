e700a12f2bdeb91366fd53827a97f8752a5ba142
Felix-code <84644592+Felix-code@users.noreply.github.com>
2022-09-21 13:35:58 +0100

    multiply numbers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * mul - multiply two intergers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: result
+ */
+
+int mul(int a, int b)
+{
+	int result = a * b;
+
+	return (result);
+}
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -1,3 +1,4 @@
 ***More functions***
 - Check if a letter is uppercase.
 - check if a number is a digit.
+- a function to multiply two integers
