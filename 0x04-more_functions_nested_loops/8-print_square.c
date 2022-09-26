73ff72c3b5b3b7cc6441e2f61df574a0f98e0940
Felix-code <84644592+Felix-code@users.noreply.github.com>
2022-09-26 11:44:34 +0100

    fixed

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_square - function that prints square
+ * @size: size of the square
+ */
+
+void print_square(int size)
+{
+	int square;
+
+	if (n > 0)
+		for (square = 0; square < size; square++)
+		{
+			_putchar('')
+		}
+
+}
